71c2fcb7876911b587ac74ccaa00d8a465bb9256
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-03-05 02:23:24 +0100

    add next js to project

---
+++ portfolio/src/app/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import Box from "@chakra-ui/react";
+
+const index = () => {
+  return (
+    <Box>
+      <Head>
+        <title>Home</title>
+      </Head>
+    </Box>
+  );
+};
+
+export default index;
